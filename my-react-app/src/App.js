6f1d365a75ff68565a7522951b54a154894b76aa
saradomi <115647886+saradomi@users.noreply.github.com>
2023-12-22 11:16:05 +0000

    First changes to main page

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './images/cool-doggo_square-paper-04.png';
 import './App.css';
 
 function App() {
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          A cool doggo is coming to you soon.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.instagram.com/guidasmoranes"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Meet the creator
         </a>
       </header>
     </div>
